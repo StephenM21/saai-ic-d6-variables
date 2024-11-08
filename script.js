a0591d0c1089cf3173a3488fe53690ad5a8b2a75
StephenM21 <stephenmoore81321@gmail.com>
2024-11-08 04:09:48 +0000

    added JavaScript

---
+++ script.js
@@ -0,0 +1,21 @@
+let animal = "siberian tiger"
+
+console.log("I'm now running JavaScript")
+console.log("Hello")
+console.log(animal)
+console.log("him")
+
+//string
+let personName = "Stephen"
+console.log(personName)
+
+//Number
+let purchasePrice = 14.97
+console.log("Original Price", purchasePrice)
+
+let purchasePrice = 9.99
+console.log("sale value", purchasePrice)
+
+//Boolean
+let isTuesday = false
+console.log(isTuesday)
